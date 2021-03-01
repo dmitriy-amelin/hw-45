00400f218cc59178f8dd758ff0e74b33c72fba04
Reey <trommxxx@mail.ru>
2021-03-01 23:59:35 +0600

    Добавляем путь для отображения задачи

--- hw_45/hw_45/urls.py
+++ hw_45/hw_45/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from to_do_list.views import index_view, task_create_view
+from to_do_list.views import index_view, task_create_view, task_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view),
-    path('task/add/', task_create_view)
+    path('task/add/', task_create_view),
+    path('task/', task_view),
 ]
