bb5635b11a5c0f5748133e7ad63195d153d16946
Reey <trommxxx@mail.ru>
2021-03-01 13:02:14 +0600

    Создана модель ToDoList

--- hw_45/to_do_list/models.py
+++ hw_45/to_do_list/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDoList(models.Model):
+    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
+    description = models.TextField(max_length=3000, null=False, blank=False)
+    status = models.CharField(max_length=300, null=False, blank=False, default='New', verbose_name='Статус')
+    date_of_completion = models.DateField(auto_now_add=False,
+                                          editable=True, null=True, blank=True, verbose_name='Дата выполнения')
+
+    class Meta:
+        db_table = 'to_do_list'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'{self.description}: {self.status}, {self.date_of_completion}'
