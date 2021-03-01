191098c6e6e32145c86ba88e591d72325ea5de97
Reey <trommxxx@mail.ru>
2021-03-01 14:54:52 +0600

    В представлении создания задачи. Добавляем метод Post

--- hw_45/to_do_list/views.py
+++ hw_45/to_do_list/views.py
@@ -15,4 +15,20 @@ def index_view(request):
 def task_create_view(request):
     if request.method == 'GET':
         return render(request, 'task_create.html', {"choices": status_choices})
+    elif request.method == 'POST':
+        description = request.POST.get("description")
+        status = request.POST.get("status")
+        author = request.POST.get("author")
+        date_of_completion = request.POST.get("date_of_completion")
 
+        task = ToDoList.objects.create(
+            description=description,
+            status=status,
+            author=author,
+            date_of_completion=date_of_completion
+        )
+        context = {
+            'task': task
+        }
+
+        return render(request, 'index.html', context)
