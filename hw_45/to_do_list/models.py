502be9719e1fd84208ba359844802f8d8dcd9507
Reey <trommxxx@mail.ru>
2021-03-01 14:46:47 +0600

    В модель добавлена константа и в status добавлено свойство choices

--- hw_45/to_do_list/models.py
+++ hw_45/to_do_list/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
 
 class ToDoList(models.Model):
     author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
     description = models.TextField(max_length=3000, null=False, blank=False)
-    status = models.CharField(max_length=300, null=False, blank=False, default='New', verbose_name='Статус')
+    status = models.CharField(max_length=40, choices=status_choices, default='new', verbose_name='Статус')
     date_of_completion = models.DateField(auto_now_add=False,
                                           editable=True, null=True, blank=True, verbose_name='Дата выполнения')
 
