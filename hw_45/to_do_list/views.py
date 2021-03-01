d98b9a80dc3aeff283be462832bb4a8d208a1d5e
Reey <trommxxx@mail.ru>
2021-03-01 13:20:42 +0600

    Добавлено представление списка задач на главную страницу

--- hw_45/to_do_list/views.py
+++ hw_45/to_do_list/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from to_do_list.models import ToDoList
+
+
+def index_view(request):
+    tasks = ToDoList.objects.all()
+    context = {
+        'tasks': tasks
+    }
+
+    return render(request, 'index.html', context)
