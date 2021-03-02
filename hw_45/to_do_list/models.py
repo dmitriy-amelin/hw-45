d8375de852c234606895f2bd84c470db2fc5e30d
Reey <trommxxx@mail.ru>
2021-03-02 22:47:26 +0600

    Начало домашнего задания №46 В модель задачи добавлено поле для подробного описания

--- hw_45/to_do_list/models.py
+++ hw_45/to_do_list/models.py
@@ -4,11 +4,12 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 
 class ToDoList(models.Model):
-    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Автор')
-    description = models.TextField(max_length=3000, null=False, blank=False)
+    author = models.CharField(max_length=40, default='Unknown', verbose_name='Автор')
+    description = models.TextField(verbose_name='Краткое описание')
+    full_description = models.TextField(blank=True, verbose_name='Полное описание')
     status = models.CharField(max_length=40, choices=status_choices, default='new', verbose_name='Статус')
     date_of_completion = models.DateField(auto_now_add=False,
-                                          editable=True, null=True, blank=True, verbose_name='Дата выполнения')
+                                          null=True, blank=True, verbose_name='Дата выполнения')
 
     class Meta:
         db_table = 'to_do_list'
