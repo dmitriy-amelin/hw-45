048bac8596d88d4da8030dcb19bc1ec5ecc409f3
Reey <trommxxx@mail.ru>
2021-03-03 01:08:51 +0600

    Аргументам name в функциях path присваиваем имена Для просмотра определенной статьи, в качестве адреса указываем <int:pk>, где через двоеточие указан тип переменной и её название

--- hw_45/to_do_list/urls.py
+++ hw_45/to_do_list/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from to_do_list.views import index_view, task_create_view, task_view
 
 urlpatterns = [
-    path('', index_view),
-    path('task/add/', task_create_view),
-    path('task/', task_view),
+    path('', index_view, name='task-list'),
+    path('task/add/', task_create_view, name='task-add'),
+    path('task/<int:pk>', task_view, name='task-view'),
 ]
